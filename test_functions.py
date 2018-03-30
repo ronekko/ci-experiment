9158bf6470e07954ab531454c2d1115ccbf00f56
ronekko <ronekko@gmail.com>
2018-03-30 15:08:05 +0900

    Add test

--- test_functions.py
+++ test_functions.py
@@ -7,11 +7,19 @@ import functions
 
 class TestSquare(unittest.TestCase):
 
+    def check_square(self, x, y_desired):
+        y_actual = functions.square(x)
+        np.testing.assert_equal(y_desired, y_actual)
+
     def test_scalar(self):
         x = 1
         y_desired = 1
-        y_actual = functions.square(x)
-        np.testing.assert_equal(y_desired, y_actual)
+        self.check_square(x, y_desired)
+
+    def test_ndarray(self):
+        x = np.array([0, 1, 2, 3], dtype=np.float32)
+        y_desired = np.array([0, 1, 4, 9], dtype=np.float32)
+        self.check_square(x, y_desired)
 
 
 if __name__ == '__main__':
